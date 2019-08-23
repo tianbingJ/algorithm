9a359dffce995093efb744323d8ca67e20a03d3e
tianbing <tbbd426@gmail.com>
2019-08-24 00:12:00 +0800

    add LIS nlogn alog

--- leetcode/300.go
+++ leetcode/300.go
@@ -5,7 +5,7 @@ package leetcode
 执行用时 :8 ms, 在所有 Go 提交中击败了67.72%的用户
 内存消耗 :2.4 MB, 在所有 Go 提交中击败了52.38%的用户
  */
-func lengthOfLIS(nums []int) int {
+func lengthOfLIS1(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
@@ -31,5 +31,33 @@ func lengthOfLIS(nums []int) int {
 
 
 /**
- NlgN的解法？
+ NlgN的解法
+非常有技巧的解法
+参考 https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
  */
+
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	tails := make([]int, len(nums))
+	size := 0
+	for _, num := range nums {
+		i := 0
+		j := size
+		for ; i != j; {
+			m := (i + j) / 2
+			if tails[m] < num {
+				i = m + 1
+			} else {
+				j = m
+			}
+		}
+		tails[i] = num
+		if i == size {
+			size ++
+		}
+	}
+	return size
+}
