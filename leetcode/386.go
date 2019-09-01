ff4873c07a051d6875969fb709c936cc6d2e673b
tianbing <tbbd426@gmail.com>
2019-09-01 22:22:10 +0800

    add comment

--- leetcode/386.go
+++ leetcode/386.go
@@ -1,10 +1,10 @@
 package leetcode
 
-import "fmt"
-
 /**
 执行用时 :2440 ms, 在所有 Go 提交中击败了94.29%的用户
 内存消耗 :27.3 MB, 在所有 Go 提交中击败了25.00%的用户
+
+处理当前数字curNum，首先把它加入list中，再需要考虑以它开头的其他数字（(curNum + i)* 10）, i = 0, ..., 9
  */
 
 type LexOrder struct  {
@@ -25,15 +25,11 @@ func lexicalOrder(n int) []int {
 	return lexOrder.result
 }
 
-/**
-每次方法调用填充完相同位数的数字
-*/
 func iterLexOrder(curNum int, order * LexOrder) {
 	if curNum > order.n  {
 		return
 	}
 	for i := 0; i <= 9 && curNum + i <= order.n && order.index < order.n; i ++ {
-		fmt.Println(curNum + i, order.index)
 		order.result[order.index] = curNum + i
 		order.index ++
 		iterLexOrder((curNum + i) * 10, order)
