6a7766797ab98ae2a247569500a26d709dc7324d
tianbing <tbbd426@gmail.com>
2019-09-22 08:57:56 +0800

    395

--- leetcode/395.go
+++ leetcode/395.go
@@ -9,7 +9,7 @@ O(n * n)
 执行用时 :512 ms, 在所有 Go 提交中击败了26.67%的用户
 内存消耗 :4.5 MB, 在所有 Go 提交中击败了7.14%的用户
 */
-func longestSubstring(s string, k int) int {
+func longestSubstring1(s string, k int) int {
 	n := len(s)
 	if n <= 0 {
 		return 0
@@ -45,3 +45,46 @@ func longestSubstring(s string, k int) int {
 	return max
 }
 
+
+func longestSubstring(s string, k int) int {
+	n := len(s)
+	if n <= 0 {
+		return 0
+	}
+	dp := make([][26]int, n)
+	dp[0][s[0]-'a'] ++
+	for i := 1; i < n; i++ {
+		copy(dp[i][:], dp[i-1][:])
+		dp[i][s[i]-'a'] ++
+	}
+
+	return doLongestSubstring(s, k, 0, n - 1)
+}
+
+func doLongestSubstring(s string, k int, start, end int) int {
+	if end - start + 1 < k {
+		return 0
+	}
+	cnt := make([]int, 26)
+	for i := start; i <= end; i ++ {
+		cnt[s[i] - 'a'] ++
+	}
+
+	allValid := true
+	for i := start; i <= end; i ++ {
+		if cnt[s[i] - 'a'] < k {
+			allValid = false
+		}
+	}
+
+	if allValid {
+		return end - start + 1
+	}
+
+	for i := start; i <= end; i ++ {
+		if cnt[s[i] - 'a'] < k {
+			return  max(doLongestSubstring(s, k, start, i - 1), doLongestSubstring(s, k, i + 1, end))
+		}
+	}
+	return end - start + 1
+}
